e58bd96a738713496524c507faee1b0f39d266ab
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-05-17 19:25:42 +0200

    change to standard retrofit download

--- app/src/main/java/com/mieczkowskidev/padawanapp/MainActivity.java
+++ app/src/main/java/com/mieczkowskidev/padawanapp/MainActivity.java
@@ -7,27 +7,19 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
-    List<Person> rxFullList = new ArrayList<>();
+//    List<Person> rxFullList = new ArrayList<>();
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String URL = "http://www.swapi.co/api/";
@@ -39,8 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
 
-//        prepareRetrofit();
-        prepareFullDownload();
+        prepareRetrofit();
+
+
+        // RX CHAIN TO DOWNLOAD IT ALL
+//        prepareFullDownload();
 
     }
 
@@ -84,64 +79,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void prepareFullDownload() {
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-
-        final SwapiService swapiService = retrofit.create(SwapiService.class);
-
-        String[] myItems = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-        Observable
-                .from(Arrays.asList(myItems))
-                .flatMap(new Func1<String, Observable<ResultsResponse>>() {
-                    @Override
-                    public Observable<ResultsResponse> call(String s) {
-                        Log.d(TAG, "call for " + s);
-                        return swapiService.getFullPeople(s);
-                    }
-                })
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<ResultsResponse>() {
-                    @Override
-                    public void onCompleted() {
-                        Log.d(TAG, "onCompleted()");
-
-                        if (rxFullList != null && !rxFullList.isEmpty()) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    prepareRecyclerView(rxFullList);
-                                }
-                            });
-                        }
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, "onError() " + e.getMessage(), e);
-
-                    }
-
-                    @Override
-                    public void onNext(ResultsResponse resultsResponse) {
-                        Log.d(TAG, "onNext response");
-
-                        if (resultsResponse.results != null && !resultsResponse.results.isEmpty()) {
-                            rxFullList.addAll(resultsResponse.results);
-                        }
-
-                    }
-                });
-
-    }
-
     private void prepareRecyclerView(List<Person> personList) {
 
         PersonAdapter personAdapter = new PersonAdapter(personList);
@@ -151,4 +88,66 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(personAdapter);
 
     }
+
+
+
+    // RX CHAIN TO DOWNLOAD IT ALL
+//
+//    private void prepareFullDownload() {
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+//                .build();
+//
+//        final SwapiService swapiService = retrofit.create(SwapiService.class);
+//
+//        String[] myItems = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+//
+//        Observable
+//                .from(Arrays.asList(myItems))
+//                .flatMap(new Func1<String, Observable<ResultsResponse>>() {
+//                    @Override
+//                    public Observable<ResultsResponse> call(String s) {
+//                        Log.d(TAG, "call for " + s);
+//                        return swapiService.getFullPeople(s);
+//                    }
+//                })
+//                .subscribeOn(Schedulers.newThread())
+////                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Subscriber<ResultsResponse>() {
+//                    @Override
+//                    public void onCompleted() {
+//                        Log.d(TAG, "onCompleted()");
+//
+//                        if (rxFullList != null && !rxFullList.isEmpty()) {
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    prepareRecyclerView(rxFullList);
+//                                }
+//                            });
+//                        }
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.e(TAG, "onError() " + e.getMessage(), e);
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(ResultsResponse resultsResponse) {
+//                        Log.d(TAG, "onNext response");
+//
+//                        if (resultsResponse.results != null && !resultsResponse.results.isEmpty()) {
+//                            rxFullList.addAll(resultsResponse.results);
+//                        }
+//
+//                    }
+//                });
+//
+//    }
 }
